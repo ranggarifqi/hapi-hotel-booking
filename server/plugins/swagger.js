3024fb44df446e94b942e29a5f201d01194d461f
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-11-29 10:51:14 +0700

    Setup JWT Auth for Swagger

--- server/plugins/swagger.js
+++ server/plugins/swagger.js
@@ -1,25 +1,33 @@
-'use strict';
+"use strict";
 
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const HapiSwagger = require('hapi-swagger');
-const Package = require('../../package.json');
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const HapiSwagger = require("hapi-swagger");
+const Package = require("../../package.json");
 
 module.exports = {
-    name: 'app-swagger',
-    async register(server) {
-
-        await server.register([
-            Inert,
-            Vision,
-            {
-                plugin: HapiSwagger,
-                options: {
-                    info: {
-                        version: Package.version
-                    }
-                }
+  name: "app-swagger",
+  async register(server) {
+    await server.register([
+      Inert,
+      Vision,
+      {
+        plugin: HapiSwagger,
+        options: {
+          info: {
+            title: "Hotel Booking API Documentation",
+            version: Package.version
+          },
+          securityDefinitions: {
+            jwt: {
+              type: "apiKey",
+              name: "Authorization",
+              in: "header"
             }
-        ]);
-    }
+          },
+          security: [{ jwt: [] }]
+        }
+      }
+    ]);
+  }
 };
